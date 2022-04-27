3aada2ee75a9293139acc835b9cab7f5ffcb4b30
Himanshu Kumar <52701083+ansh4223@users.noreply.github.com>
2022-04-27 13:23:17 +0530

    Added solution - LeetHub

---
+++ Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    string longestPalin (string str) {
+        // code here
+    int n = str.size(); 
+    int maxLength = 1,start=0;
+    int low, high;
+    for (int i = 0; i < n; i++) {
+        low = i - 1;
+        high = i + 1;
+        while ( high < n && str[high] == str[i])                                
+            high++;
+       
+        while ( low >= 0 && str[low] == str[i])                   
+            low--;
+       
+        while (low >= 0 && high < n && str[low] == str[high]){
+              low--;
+            high++;
+        }
+ 
+        int length = high - low - 1;
+        if (maxLength < length) {
+            maxLength = length;
+              start=low+1;
+        }
+    }
+    return str.substr(start,maxLength);;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+        
+        Solution ob;
+        cout << ob.longestPalin (S) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+  // } Driver Code Ends
